dc48c5796e26f0e2a127595c857aef88d462fb01
Milad Khakpour <milad.khakpour@gmail.com>
2015-12-01 14:41:17 +0100

    put them in a function

--- projection.py
+++ projection.py
@@ -2,14 +2,22 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-readPath = "data_sample/"
-savePath = "data_sample/wgs84"
-if not os.path.exists(savePath):
-    os.makedirs(savePath)
 
-onlyFiles = [f for f in listdir(readPath) if isfile(join(readPath, f))]
+# Convert to WGS84
+def reprojection(filesPath):
 
-for fName in onlyFiles:
-    dstnPath = os.path.join(savePath, 'WGS84_'+fName)
-    os.system('gdalwarp %s %s -t_srs "+proj=longlat +ellps=WGS84"'
-              % (readPath+fName, dstnPath))
+    readPath = filesPath
+    savePath = filesPath + "/wgs84"
+
+    # Create folder if savePath doesn't exist
+    if not os.path.exists(savePath):
+        os.makedirs(savePath)
+
+    # Collect all files tiff files and put them in a list
+    onlyFiles = [f for f in listdir(readPath) if isfile(join(readPath, f))]
+
+    # Convert each file into WGS84 with gdalwarp
+    for fName in onlyFiles:
+        dstnPath = os.path.join(savePath, 'WGS84_'+fName)
+        os.system('gdalwarp %s %s -t_srs "+proj=longlat +ellps=WGS84"'
+                  % (readPath+fName, dstnPath))
